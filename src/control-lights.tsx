a9bd175d2fdbfd51e2a9e32d2a9279f3df9a3d18
Collin Henderson <collinpatrickhenderson@gmail.com>
2023-10-15 23:26:03 -0400

    Adds ability to add custom brightness values

--- src/control-lights.tsx
+++ src/control-lights.tsx
@@ -1,6 +1,20 @@
-import { Action, ActionPanel, Color, Icon, List, showToast, Toast, launchCommand, LaunchType } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  Color,
+  Icon,
+  List,
+  showToast,
+  Toast,
+  launchCommand,
+  LaunchType,
+  Form,
+  useNavigation,
+  confirmAlert,
+  Alert,
+} from "@raycast/api";
 import { useDevice } from "./hooks/use-device";
-import { useCachedState } from "@raycast/utils";
+import { showFailureToast, useCachedState } from "@raycast/utils";
 import { useState } from "react";
 
 export default function Command() {
@@ -15,6 +29,7 @@ export default function Command() {
     updateDeviceEffect,
   } = useDevice();
   const [effects, setEffects] = useCachedState<string[]>("device-effects", deviceMetadata?.effects.effectsList ?? []);
+  const [customBrightnessValues, setCustomBrightnessValues] = useCachedState<number[]>("custom-brightness-values", []);
   const [isLoadingEffects, setIsLoadingEffects] = useState<boolean>(false);
 
   async function doSetDeviceBrightness(brightness: number) {
@@ -36,6 +51,30 @@ export default function Command() {
 
   async function launchPairDeviceCommand() {
     await launchCommand({ name: "pair-device", type: LaunchType.UserInitiated });
+  }
+
+  async function handleAddCustomBrightnessValue(value: number) {
+    if (customBrightnessValues.includes(value)) {
+      showFailureToast("That value already exists.");
+      return;
+    }
+
+    setCustomBrightnessValues((vals) => [...vals, value]);
+    await showToast({ title: "Custom brightness value added successfully", style: Toast.Style.Success });
+  }
+
+  async function clearCustomBrightnessValues() {
+    if (
+      await confirmAlert({
+        title: "Are you sure you want to delete all of your custom brightness values?",
+        primaryAction: {
+          title: "Yes, Delete Them",
+          style: Alert.ActionStyle.Destructive,
+        },
+      })
+    ) {
+      setCustomBrightnessValues([]);
+    }
   }
 
   return (
@@ -95,6 +134,17 @@ export default function Command() {
             actions={
               <ActionPanel>
                 <ActionPanel.Submenu title="Select a Brightness">
+                  {!!customBrightnessValues.length && (
+                    <ActionPanel.Section title="Custom Values">
+                      {customBrightnessValues.map((value) => (
+                        <Action
+                          title={`${value}% Brightness`}
+                          key={value}
+                          onAction={() => doSetDeviceBrightness(value)}
+                        />
+                      ))}
+                    </ActionPanel.Section>
+                  )}
                   {[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map((brightness) => (
                     <Action
                       title={`${brightness}% Brightness`}
@@ -103,6 +153,19 @@ export default function Command() {
                     />
                   ))}
                 </ActionPanel.Submenu>
+                <Action.Push
+                  title="Add Custom Brightness"
+                  icon={Icon.PlusCircle}
+                  target={<CustomBrightnessForm onAddCustomBrightnessValue={handleAddCustomBrightnessValue} />}
+                />
+                {!!customBrightnessValues.length && (
+                  <Action
+                    title="Clear Custom Brightness List"
+                    icon={{ source: Icon.XMarkCircle, tintColor: Color.Red }}
+                    onAction={clearCustomBrightnessValues}
+                    style={Action.Style.Destructive}
+                  />
+                )}
               </ActionPanel>
             }
           />
@@ -123,5 +186,57 @@ export default function Command() {
         />
       )}
     </List>
+  );
+}
+
+interface CustomBrightnessFormProps {
+  onAddCustomBrightnessValue: (value: number) => void;
+}
+
+function CustomBrightnessForm({ onAddCustomBrightnessValue }: CustomBrightnessFormProps) {
+  const { pop } = useNavigation();
+  const [value, setValue] = useState<string>("50");
+  const [validationError, setValidationError] = useState<string>("");
+
+  function validateValue(e: Form.Event<string>) {
+    const value = parseInt(e.target.value ?? "0", 10);
+
+    if (isNaN(value) || value < 0 || value > 100) {
+      setValidationError("Please enter a valid number between 0 and 100");
+    } else {
+      setValidationError("");
+    }
+  }
+
+  function handleAddCustomBrightnessValue({ value }: { value: string }) {
+    const numberValue = parseInt(value, 10);
+
+    if (isNaN(numberValue) || numberValue < 0 || numberValue > 100) {
+      setValidationError("Please enter a valid number between 0 and 100");
+    } else {
+      setValidationError("");
+      onAddCustomBrightnessValue(numberValue);
+      pop();
+    }
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm onSubmit={handleAddCustomBrightnessValue} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField
+        id="value"
+        title="Value"
+        info="Must be a number between 0 and 100"
+        value={value}
+        onChange={setValue}
+        onBlur={validateValue}
+        error={validationError}
+      />
+    </Form>
   );
 }
