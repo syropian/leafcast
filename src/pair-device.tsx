c4aa7e32da524859946f0a109d5c2d5a53e03cc2
Collin Henderson <collinpatrickhenderson@gmail.com>
2026-05-22 15:17:33 -0400

    Pull contributions

--- src/pair-device.tsx
+++ src/pair-device.tsx
@@ -1,34 +1,95 @@
-import { LocalStorage, showToast, Toast, getPreferenceValues, confirmAlert } from "@raycast/api";
+import {
+  showToast,
+  Toast,
+  confirmAlert,
+  List,
+  Icon,
+  Image,
+  Color,
+  useNavigation,
+  ActionPanel,
+  Action,
+} from "@raycast/api";
 import { showFailureToast } from "@raycast/utils";
-import axios from "axios";
+import { useDeviceApi } from "./hooks/use-device-api";
+import { useEffect, useState } from "react";
+import { SetDeviceAddressForm } from "./components/SetDeviceAddressForm";
 
-interface Preferences {
-  deviceAddress: string;
-}
+export default function Command() {
+  const { deviceAddress, pairDevice, setDeviceAddress } = useDeviceApi();
+  const [status, setStatus] = useState("Waiting to pair...");
+  const [message, setMessage] = useState("");
+  const [icon, setIcon] = useState<Image.ImageLike>(Icon.Clock);
+  const { pop } = useNavigation();
 
-export default async function Command() {
-  if (
-    await confirmAlert({
-      title: "Place your device in pairing mode",
-      message:
-        "Before we begin pairing you must put your device into pairing mode. To do this, press and hold the power button on your device until the LED starts flashing. ",
-      primaryAction: {
-        title: "I'm ready to pair",
-      },
-    })
-  ) {
-    const toast = await showToast({ title: "Authorizing device...", style: Toast.Style.Animated });
-    const { deviceAddress } = getPreferenceValues<Preferences>();
-    try {
-      const response = await axios.post(`http://${deviceAddress}:16021/api/v1/new`);
-      const token = response.data.auth_token;
-      await LocalStorage.setItem("device-token", token);
-      toast.title = "Device successfully paired";
-      toast.style = Toast.Style.Success;
-    } catch (e) {
-      showFailureToast("Ensure device is in pairing mode", {
-        title: "Pairing failed",
-      });
+  useEffect(() => {
+    if (deviceAddress) attemptToPairDevice();
+  }, [deviceAddress]);
+
+  async function attemptToPairDevice() {
+    if (
+      await confirmAlert({
+        title: "Place your device in pairing mode",
+        message:
+          "Before we begin pairing you must put your device into pairing mode. To do this, press and hold the power button on your device until the LED starts flashing.",
+        primaryAction: {
+          title: "I'm ready to pair",
+        },
+      })
+    ) {
+      const toast = await showToast({ title: "Fetching token...", style: Toast.Style.Animated });
+
+      setStatus("Fetching token...");
+      setIcon(Icon.Signal3);
+
+      try {
+        await pairDevice();
+
+        toast.title = "Device successfully paired!";
+
+        toast.style = Toast.Style.Success;
+
+        setStatus("Device successfully paired!");
+        setIcon({ source: Icon.CheckCircle, tintColor: Color.Green });
+
+        pop();
+      } catch (e) {
+        showFailureToast("Ensure device is in pairing mode", {
+          title: "Pairing failed",
+        });
+        setStatus("Pairing failed");
+        setMessage(
+          "Please ensure you're both using the correct IPv4 address and have the device in pairing mode and try again."
+        );
+        setIcon({ source: Icon.XMarkCircle, tintColor: Color.Red });
+      }
+    } else {
+      setStatus("Device pairing cancelled");
+      setIcon(Icon.MinusCircle);
     }
   }
+
+  function resetDeviceAddress() {
+    setDeviceAddress("");
+  }
+
+  return deviceAddress ? (
+    <List
+      actions={
+        <ActionPanel>
+          <Action title="Pair Device" onAction={attemptToPairDevice} icon={Icon.Plug} />
+          <Action
+            title="Reset Device Address"
+            style={Action.Style.Destructive}
+            onAction={resetDeviceAddress}
+            icon={Icon.XMarkCircle}
+          />
+        </ActionPanel>
+      }
+    >
+      <List.EmptyView title={status} icon={icon} description={message} />
+    </List>
+  ) : (
+    <SetDeviceAddressForm onSetDeviceAddress={setDeviceAddress} currentDeviceAddress={deviceAddress} />
+  );
 }
